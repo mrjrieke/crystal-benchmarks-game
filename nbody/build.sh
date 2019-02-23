08d3e3f839d81476af3efe44476b070c1c90021d
mrjrieke <mrjrieke@gmail.com>
2019-02-23 14:37:36 -0800

    Adding go and java.

--- nbody/build.sh
+++ nbody/build.sh
@@ -1,2 +1,4 @@
 gcc -O3 -fomit-frame-pointer -funroll-loops nbody.c -o bin_c -lm
 crystal build nbody.cr --release -o bin_cr
+go build -o bin_go nbody.go
+javac nbody.java
--- nbody/run.sh
+++ nbody/run.sh
@@ -2,5 +2,7 @@ echo Crystal
 ../xtime.rb ./bin_cr 10000000
 echo C
 ../xtime.rb ./bin_c 10000000
-echo Ruby
-../xtime.rb ruby nbody.rb 10000000
+echo Java
+../xtime.rb java nbody 10000000
+echo Go
+../xtime.rb ./bin_go 10000000
