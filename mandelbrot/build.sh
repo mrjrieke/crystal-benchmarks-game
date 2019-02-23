eb57d7ae3e54236aa2bc5b2b5512b13707d26e8f
mrjrieke <mrjrieke@gmail.com>
2019-02-23 14:27:05 -0800

    Adding java and go to mandelbrot.

--- mandelbrot/build.sh
+++ mandelbrot/build.sh
@@ -1,2 +1,4 @@
 gcc -O3 -ffast-math -fomit-frame-pointer -funroll-loops mandelbrot.c -o bin_c
 crystal build mandelbrot.cr --release -o bin_cr
+javac mandelbrot.java
+go build -o bin_go mandelbrot.go
--- mandelbrot/run.sh
+++ mandelbrot/run.sh
@@ -2,5 +2,7 @@ echo Crystal
 ../xtime.rb ./bin_cr 10000 > /dev/null
 echo C
 ../xtime.rb ./bin_c 10000 > /dev/null
-echo Ruby
-../xtime.rb ruby mandelbrot.rb 5000 > /dev/null
+echo Go
+../xtime.rb ./bin_go 10000 > /dev/null
+echo Java
+../xtime.rb java mandelbrot 10000 > /dev/null
