84ea136d94664a95c8cee07f3022daca0e92d5f8
mrjrieke <mrjrieke@gmail.com>
2019-02-23 15:07:24 -0800

    Adding spectral norm.

--- spectralnorm/build.sh
+++ spectralnorm/build.sh
@@ -1,2 +1,4 @@
 gcc -O3 -fomit-frame-pointer -funroll-loops spectralnorm.c -o bin_c -lm
 crystal build spectralnorm.cr --release -o bin_cr
+javac spectralnorm.java
+go build -o bin_go spectralnorm.go
--- spectralnorm/run.sh
+++ spectralnorm/run.sh
@@ -2,5 +2,10 @@ echo Crystal
 ../xtime.rb ./bin_cr 5000
 echo C
 ../xtime.rb ./bin_c 5000
-echo Ruby
-../xtime.rb ruby spectralnorm.rb 5000
+#echo Ruby
+#../xtime.rb ruby spectralnorm.rb 5000
+echo Java
+../xtime.rb java spectralnorm 5000
+echo Go
+../xtime.rb ./bin_go 5000
+
