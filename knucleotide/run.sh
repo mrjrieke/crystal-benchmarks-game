1ddcc099f55e9df86387792f72f8a58f4bf19045
mrjrieke <mrjrieke@gmail.com>
2019-02-23 14:20:50 -0800

    Adding java for knucleotide.

--- knucleotide/run.sh
+++ knucleotide/run.sh
@@ -2,5 +2,5 @@ echo Crystal
 cat 1.txt | ../xtime.rb ./bin_cr
 echo Go
 cat 1.txt | ../xtime.rb ./bin_go
-echo Ruby
-cat 1.txt | ../xtime.rb ruby knucleotide.rb
+echo Java
+cat 1.txt | ../xtime.rb java knucleotide
