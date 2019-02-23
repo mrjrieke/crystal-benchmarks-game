e5444ddf2c7def9e3f4e8d46701ca50cd7cb9cc8
mrjrieke <mrjrieke@gmail.com>
2019-02-23 14:01:29 -0800

    Adding java and go to fasta.

--- fasta/build.sh
+++ fasta/build.sh
@@ -1,2 +1,4 @@
 gcc -O3 -fomit-frame-pointer -funroll-loops fasta.c -o bin_c
 crystal build fasta.cr --release -o bin_cr
+go build -o bin_go fasta.go
+javac fasta.java-2.java
--- fasta/run.sh
+++ fasta/run.sh
@@ -2,5 +2,7 @@ echo Crystal
 ../xtime.rb ./bin_cr 15000000 > /dev/null
 echo C
 ../xtime.rb ./bin_c 15000000 > /dev/null
-echo Ruby
-../xtime.rb ruby fasta.rb 15000000 > /dev/null
+echo Go
+../xtime.rb ./bin_go 15000000 > /dev/null
+echo Java
+../xtime.rb java fasta 15000000 > /dev/null
