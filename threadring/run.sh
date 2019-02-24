1313f268a5b3b3cc3edde71624ecce24b96fa460
mrjrieke <mrjrieke@gmail.com>
2019-02-23 17:17:34 -0800

    Disable results don't really care about at this time.

--- threadring/run.sh
+++ threadring/run.sh
@@ -3,11 +3,11 @@ echo Crystal
 ../xtime.rb ./bin_cr $NUM
 echo Go
 ../xtime.rb ./bin_go $NUM
-echo Ruby
-../xtime.rb ruby threadring.rb $NUM
-echo Erlang
-../xtime.rb erl -noshell -run threadring main $NUM
-echo Scala
-../xtime.rb scala threadring $NUM
-echo Goby
-../xtime.rb goby threadring.gb $NUM
+#echo Ruby
+#../xtime.rb ruby threadring.rb $NUM
+#echo Erlang
+#../xtime.rb erl -noshell -run threadring main $NUM
+#echo Scala
+#../xtime.rb scala threadring $NUM
+#echo Goby
+#../xtime.rb goby threadring.gb $NUM
